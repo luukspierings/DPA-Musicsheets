4dc336794b7640245367a463e4b41e0bdfe2145f
Jonathan <jgimmink@avans.nl>
2017-09-06 10:02:05 +0200

    Added models

---
+++ DPA_Musicsheets/Models/MusicNote.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.Models
+{
+    public class MusicNote
+    {
+        // Toonhoogte
+        // Octaaf
+        // Kruis. mol of niks
+        // Duur
+        // rusten
+        // punten aangeven
+        // maatsoort aangeven
+        // 
+    }
+}
---
+++ DPA_Musicsheets/Models/Staff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.Models
+{
+    class Staff
+    {
+        // maatsoort
+        // lijst met Bars
+        // treble of bass
+        // tempo
+        // 
+    }
+}
