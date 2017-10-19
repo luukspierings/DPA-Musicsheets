c1c533f8b7f2f3f3a2eacd565e6a1954f0cc038a
luukspierings <lcpspier@avans.nl>
2017-10-19 14:04:07 +0200

    Note models

---
+++ DPA_Musicsheets/Models/BaseNote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.Models
+{
+    public abstract class BaseNote
+    {
+        protected float duration;
+        public virtual float Duration { get { return duration; } set { duration = value; } }
+
+
+    }
+}
--- DPA_Musicsheets/Models/MusicNote.cs
+++ DPA_Musicsheets/Models/MusicNote.cs
@@ -6,15 +6,30 @@ using System.Threading.Tasks;
 
 namespace DPA_Musicsheets.Models
 {
-    public class MusicNote
+    public class MusicNote : BaseNote
     {
-        // Toonhoogte
-        // Octaaf
-        // Kruis. mol of niks
-        // Duur
-        // rusten
-        // punten aangeven
-        // maatsoort aangeven
-        // 
+
+        public char Pitch { get; set; }
+        public int Octave { get; set; }
+
+        public bool Dotted { get; set; }
+        public PitchModifier PitchModifier { get; set; }
+
+        public override float Duration
+        {
+            get
+            {
+                if (Dotted)
+                {
+                    return duration * 1.5f;
+                }
+
+                return duration;
+            }
+            set { duration = value; }
+        }
+
+
+
     }
 }
